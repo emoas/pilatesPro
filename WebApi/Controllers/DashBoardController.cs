6363c5eb7f880423b86a372c38ba4eead3e719d2
emoas <emoas12@gmail.com>
2024-11-02 09:11:31 -0300

    Añadir .gitignore y eliminar archivos innecesarios

--- WebApi/Controllers/DashBoardController.cs
+++ WebApi/Controllers/DashBoardController.cs
@@ -1,7 +1,7 @@
 ﻿using Commons.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using ServicesInterface;
-
+using System;
 
 namespace WebApi.Controllers
 {
@@ -14,12 +14,34 @@ namespace WebApi.Controllers
         {
             this.dashBoardService = dashBoardService;
         }
+        [HttpGet]
         public IActionResult GetHome()
         {
             try
             {
                 var dashboard = this.dashBoardService.GetHome();
                 return Ok(dashboard);
+            }
+            catch (System.ArgumentException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+            catch (ValidationException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+            catch (System.Exception exception)
+            {
+                return StatusCode(500, "Algo salió mal.");
+            }
+        }
+        [HttpGet("clasesLocalFecha/{idLocal}/{fecha}")]
+        public IActionResult GetClasesLocalFecha([FromRoute] int idLocal, DateTime fecha)
+        {
+            try
+            {
+                var agenda = this.dashBoardService.GetClasesLocalFecha(idLocal, fecha);
+                return Ok(agenda);
             }
             catch (System.ArgumentException exception)
             {
